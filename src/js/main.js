5a503d3c29be28a900793244340aab429403a134
Nutty4616 <nkosin361@gmail.com>
2022-09-28 17:04:41 +0200

    fixing build script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
+// Import all plugins
 import * as bootstrap from "bootstrap";
+
+// Or import only needed plugins
+import {
+  Tooltip as Tooltip,
+  Toast as Toast,
+  Popover as Popover,
+} from "bootstrap";
+
+import Alert from "bootstrap/js/dist/alert";
